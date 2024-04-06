bff7c98b0914f21bdb32a7f55f43243d12572fde
Petar Petrov <petizad55@gmail.com>
2024-04-06 11:07:19 +0200

    Finish challenge 1

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
 use actix_web::{
-    error, get, 
-    http::{header::ContentType, StatusCode}, 
-    web::ServiceConfig, 
-    HttpResponse};
-use shuttle_actix_web::ShuttleActixWeb;
+    error, get,
+    http::{header::ContentType, StatusCode},
+    web,
+    web::ServiceConfig,
+    HttpResponse,
+};
 use derive_more::{Display, Error};
+use shuttle_actix_web::ShuttleActixWeb;
 
 #[derive(Debug, Display, Error)]
 enum ServerError {
     #[display(fmt = "Internal Server Error")]
-    InternalError
+    InternalError,
 }
 
 impl error::ResponseError for ServerError {
@@ -21,14 +23,9 @@ impl error::ResponseError for ServerError {
 
     fn status_code(&self) -> StatusCode {
         match *self {
-            ServerError::InternalError => StatusCode::INTERNAL_SERVER_ERROR
+            ServerError::InternalError => StatusCode::INTERNAL_SERVER_ERROR,
         }
     }
-}
-
-#[get("/")]
-async fn hello_world() -> &'static str {
-    "Hello World!"
 }
 
 #[get("/-1/error")]
@@ -36,11 +33,34 @@ async fn error_page() -> Result<&'static str, ServerError> {
     Err(ServerError::InternalError)
 }
 
+#[get("/1/{ids:.*}")]
+async fn cube_bits(path: web::Path<String>) -> Result<HttpResponse, ServerError> {
+    let ids = path.into_inner();
+
+    print!("{}", ids);
+
+    let nums: Vec<i64> = ids
+        .split('/')
+        .map(|n| return n.parse::<i64>().unwrap_or(0))
+        .collect();
+
+    let mut res = 0;
+
+    for num in nums {
+        res = res ^ num;
+    }
+
+    res = res.pow(3);
+
+    Ok(HttpResponse::Ok().body(res.to_string()))
+}
+
+
 #[shuttle_runtime::main]
 async fn main() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
     let config = move |cfg: &mut ServiceConfig| {
-        cfg.service(hello_world);
         cfg.service(error_page);
+        cfg.service(cube_bits);
     };
 
     Ok(config.into())
