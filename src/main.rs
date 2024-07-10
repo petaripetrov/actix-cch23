6db6bd628d1d68129b24e38ef062bd1c658e04b7
Petar Petrov <petizad55@gmail.com>
2024-07-10 22:16:27 +0300

    task 4 and some misc changes

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,73 @@
 use actix_web::{
     error, get,
     http::{header::ContentType, StatusCode},
-    web,
-    web::ServiceConfig,
+    post,
+    web::{self, ServiceConfig},
     HttpResponse,
 };
 use derive_more::{Display, Error};
+use serde::{Deserialize, Serialize};
 use shuttle_actix_web::ShuttleActixWeb;
+
+#[derive(PartialEq, PartialOrd, Deserialize, Clone, Copy)]
+struct Speed(f64);
+
+impl Speed {
+    fn new(val: f64) -> Option<Speed> {
+        if val.is_nan() {
+            None
+        } else {
+            Some(Speed(val))
+        }
+    }
+}
+
+impl Default for Speed {
+    fn default() -> Self {
+        Speed(0.0)
+    }
+}
+
+impl Eq for Speed {}
+
+impl Ord for Speed {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.partial_cmp(other).unwrap()
+    }
+}
+
+impl std::fmt::Display for Speed {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+#[derive(Deserialize)]
+struct Deer {
+    name: String,
+    strength: i64,
+    #[serde(default)]
+    speed: Speed,
+    #[serde(default)]
+    height: i64,
+    #[serde(default)]
+    antler_width: i64,
+    #[serde(default)]
+    snow_magic_power: i64,
+    #[serde(default)]
+    favorite_food: String,
+    #[serde(default)]
+    #[serde(rename(serialize = "cAnD13s_3ATeN-yesT3rdAy"))]
+    candies_eaten_yesterday: i64,
+}
+
+#[derive(Serialize)]
+struct DeerResponse {
+    fastest: String,
+    tallest: String,
+    magician: String,
+    consumer: String,
+}
 
 #[derive(Debug, Display, Error)]
 enum ServerError {
@@ -55,12 +116,42 @@ async fn cube_bits(path: web::Path<String>) -> Result<HttpResponse, ServerError>
     Ok(HttpResponse::Ok().body(res.to_string()))
 }
 
+#[post("/4/strength")]
+async fn strength(deer: web::Json<Vec<Deer>>) -> Result<HttpResponse, ServerError> {
+    let strength: i64 = deer.iter().map(|d| d.strength).sum();
+
+    Ok(HttpResponse::Ok().body(strength.to_string()))
+}
+
+#[post("/4/contest")]
+async fn contest(deer: web::Json<Vec<Deer>>) -> Result<HttpResponse, ServerError> {
+    let deer_iter = deer.iter();
+
+    let fastest = deer_iter.clone().max_by_key(|d| d.speed).unwrap();
+    let tallest = deer_iter.clone().max_by_key(|d| d.height).unwrap();
+    let magician = deer_iter
+        .clone()
+        .max_by_key(|d| d.snow_magic_power)
+        .unwrap();
+    let consumer = deer_iter.max_by_key(|d| d.candies_eaten_yesterday).unwrap();
+
+    let response = DeerResponse {
+        fastest: format!("Speeding past the finish line with a strength of {0} is {1}", fastest.strength, fastest.name),
+        tallest: format!("{0} is standing tall with his {1} cm wide antlers", tallest.name, tallest.antler_width),
+        magician: format!("{0} could blast you away with a snow magic power of {1}", magician.name, magician.snow_magic_power),
+        consumer: format!("{0} ate lots of candies, but also some {1}", consumer.name, consumer.favorite_food),
+    };
+    
+    Ok(HttpResponse::Ok().json(response))
+}
 
 #[shuttle_runtime::main]
 async fn main() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
     let config = move |cfg: &mut ServiceConfig| {
         cfg.service(error_page);
         cfg.service(cube_bits);
+        cfg.service(strength);
+        cfg.service(contest);
     };
 
     Ok(config.into())
