1ccd4b5eade361d3e9a46d349bff72a91a6a2eb2
Petar Petrov <petizad55@gmail.com>
2024-07-14 23:00:09 +0300

    challenge 7

--- src/endpoints.rs
+++ src/endpoints.rs
@@ -1,11 +1,13 @@
 use actix_web::{
     error, get,
     http::{header::ContentType, StatusCode},
-    post, web, HttpResponse,
+    post, web, HttpRequest, HttpResponse,
 };
+use base64::{engine::general_purpose, Engine};
 use derive_more::{Display, Error};
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 use serde_json::json;
+use std::collections::HashMap;
 
 #[derive(PartialEq, PartialOrd, Deserialize, Clone, Copy)]
 struct Speed(f64);
@@ -202,6 +204,85 @@ async fn elf_on_shelf(text: String) -> Result<HttpResponse, ServerError> {
         "elf": text.matches("elf").count(),
         "elf on a shelf": elf_on_shelf,
         "shelf with no elf on it": shelf_count - elf_on_shelf
+    });
+
+    Ok(HttpResponse::Ok().json(res))
+}
+
+#[get("/7/decode")]
+async fn decode(req: HttpRequest) -> Result<HttpResponse, ServerError> {
+    let cookie = match req.cookie("recipe") {
+        Some(val) => val,
+        None => return Err(ServerError::InternalError),
+    };
+
+    let val = cookie.value();
+    let decoded = general_purpose::STANDARD.decode(val).unwrap();
+
+    Ok(HttpResponse::Ok().body(decoded))
+}
+
+// #[derive(Deserialize, Serialize)]
+// struct CookieData {
+//     flour: usize,
+//     sugar: usize,
+//     butter: usize,
+//     #[serde(rename(deserialize = "baking powder", serialize = "baking powder"))]
+//     baking_powder: usize,
+//     #[serde(rename(deserialize = "chocolate chips", serialize = "chocolate chips"))]
+//     chocolate_chips: usize,
+// }
+
+#[derive(Deserialize, Serialize)]
+struct BakingData {
+    recipe: HashMap<String, usize>,
+    pantry: HashMap<String, usize>,
+}
+
+#[get("/7/bake")]
+async fn bake(req: HttpRequest) -> Result<HttpResponse, ServerError> {
+    let cookie = match req.cookie("recipe") {
+        Some(val) => val,
+        None => return Err(ServerError::InternalError),
+    };
+
+    let val = cookie.value();
+    let decoded = general_purpose::STANDARD.decode(val).unwrap();
+
+    let baking_data: BakingData = match serde_json::from_slice(&decoded) {
+        Ok(val) => val,
+        Err(e) => {
+            println!("{}", e);
+            return Err(ServerError::InternalError);
+        }
+    };
+
+
+    let mut cookie_count: usize = usize::MAX;
+
+    // TODO possibly refactor this with fold left
+    for (ingredient, amount) in baking_data.recipe.iter() {
+        // Skip 0 amount to save time
+        // not really necessary, but good practice
+        if *amount <= 0 {
+            continue;
+        }
+        
+        cookie_count = Ord::min(cookie_count, baking_data.pantry.get(ingredient).unwrap_or(&0) / amount)
+    }
+
+    let mut pantry = baking_data.pantry;
+
+    // TODO possibly refactor this with a mapping function or something similar
+    for (ingrdient, amount) in pantry.iter_mut() {
+        if let Some(needed) = baking_data.recipe.get(ingrdient) {
+            *amount -= cookie_count * needed;
+        }
+    }
+  
+    let res = json!({
+        "cookies": cookie_count,
+        "pantry": pantry
     });
 
     Ok(HttpResponse::Ok().json(res))
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::web::ServiceConfig;
+use actix_web::web::{self, ServiceConfig};
 use shuttle_actix_web::ShuttleActixWeb;
 
 mod endpoints;
@@ -6,13 +6,18 @@ mod endpoints;
 #[shuttle_runtime::main]
 async fn actix_web() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
     let config = move |cfg: &mut ServiceConfig| {
-        cfg.service(endpoints::index_page); // maybe replace this with a page with links to the various tasks
-        cfg.service(endpoints::error_page);
-        cfg.service(endpoints::cube_bits);
-        cfg.service(endpoints::strength);
-        cfg.service(endpoints::contest);
-        cfg.service(endpoints::names_list);
-        cfg.service(endpoints::elf_on_shelf);
+        cfg.service(
+            web::scope("")
+                .service(endpoints::index_page) // maybe replace this with a page with links to the various tasks
+                .service(endpoints::error_page)
+                .service(endpoints::cube_bits)
+                .service(endpoints::strength)
+                .service(endpoints::contest)
+                .service(endpoints::names_list)
+                .service(endpoints::elf_on_shelf)
+                .service(endpoints::decode)
+                .service(endpoints::bake)
+        );
     };
 
     Ok(config.into())
